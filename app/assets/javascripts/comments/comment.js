a6d016f8e9409314497dc028518bdc079fabfcac
Jhonatan Hidalgo <jhonatan.hidalgo.kommit@gmail.com>
2022-06-20 16:44:55 -0500

    Application of change requests
    change a tag for link to and js files are deleted

--- app/assets/javascripts/comments/comment.js
+++
@@ -1,9 +0,0 @@
-/**
- * show confirm dialog and delete comment on confirm response
- */
-function delete_comment(){
-    if (confirm('are you sure?')){
-        document.getElementById("delete_comment_form").submit();
-    }
-}
-
